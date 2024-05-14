3f7a6ee741bea378d97069548fc900e5684dc838
lucas-ribard <lucas.ribard@laplateforme.io>
2024-05-14 15:49:03 +0200

    check wich os the client is beeing compiled

--- src/Client.cpp
+++ src/Client.cpp
@@ -2,26 +2,42 @@
 #include "../include/LPTF_Socket.hpp"
 
 int main() {
-    try {
-        LPTF_Socket clientSocket;
-        clientSocket.connectSocket("10.10.30.146", 8888);
-        std::cout << "(type 'exit' to quit)" << std::endl;
-        while (true) {
-            std::string msg;
-            std::cout << "Enter message : ";
-            std::getline(std::cin, msg);
+      #ifdef _WIN32
+    std::cout << "Running on Windows OS\n";
+    
+    #elif __linux__ //if on linux, run linux client
+   
+        try {
+            LPTF_Socket clientSocket;
+            clientSocket.connectSocket("127.0.0.1", 8888);
+            std::cout << "(type 'exit' to quit)" << std::endl;
+            while (true) {
+                std::string msg;
+                std::cout << "Enter message : ";
+                std::getline(std::cin, msg);
 
-            if (msg == "exit") {// Exit the loop if the user types 'exit'
-                break; 
+                if (msg == "exit") {// Exit the loop if the user types 'exit'
+                    break; 
+                }
+
+                clientSocket.sendMsg(msg); // Send the user input to the server
+                std::cout << clientSocket.receiveMsg() << std::endl; // Receive and print the response from the server
             }
-
-            clientSocket.sendMsg(msg); // Send the user input to the server
-            std::cout << clientSocket.receiveMsg() << std::endl; // Receive and print the response from the server
+            
+        } catch (const std::exception& e) {
+            std::cerr << "Client Exception: " << e.what() << std::endl;
+            return 1;
         }
-        
-    } catch (const std::exception& e) {
-        std::cerr << "Client Exception: " << e.what() << std::endl;
-        return 1;
-    }
+        return 0;
+
+
+    #elif __APPLE__
+    std::cout << "Running on Apple OS\n";
+
+    #else
+    std::cout << "Unsupported OS\n";
+
+    #endif
+
     return 0;
 }
